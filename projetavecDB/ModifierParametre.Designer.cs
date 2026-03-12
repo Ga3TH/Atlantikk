1c77af5236d9d49a0b62fe30ad0b7bdaa6c18dab
gaeth-hub <gaeth0311@gmail.com>
2026-03-12 15:05:29 +0100

    finir parametre

--- projetavecDB/ModifierParametre.Designer.cs
+++ projetavecDB/ModifierParametre.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbxInfo = new System.Windows.Forms.GroupBox();
+            this.cbxProd = new System.Windows.Forms.CheckBox();
+            this.lblmail = new System.Windows.Forms.Label();
+            this.tbxMail = new System.Windows.Forms.TextBox();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gbxInfo
+            // 
+            this.gbxInfo.Location = new System.Drawing.Point(28, 12);
+            this.gbxInfo.Name = "gbxInfo";
+            this.gbxInfo.Size = new System.Drawing.Size(352, 378);
+            this.gbxInfo.TabIndex = 0;
+            this.gbxInfo.TabStop = false;
+            this.gbxInfo.Text = "groupBox1";
+            // 
+            // cbxProd
+            // 
+            this.cbxProd.AutoSize = true;
+            this.cbxProd.Location = new System.Drawing.Point(612, 42);
+            this.cbxProd.Name = "cbxProd";
+            this.cbxProd.Size = new System.Drawing.Size(116, 17);
+            this.cbxProd.TabIndex = 1;
+            this.cbxProd.Text = "EN PRODUCTION";
+            this.cbxProd.UseVisualStyleBackColor = true;
+            // 
+            // lblmail
+            // 
+            this.lblmail.AutoSize = true;
+            this.lblmail.Location = new System.Drawing.Point(495, 92);
+            this.lblmail.Name = "lblmail";
+            this.lblmail.Size = new System.Drawing.Size(31, 13);
+            this.lblmail.TabIndex = 2;
+            this.lblmail.Text = "mail :";
+            // 
+            // tbxMail
+            // 
+            this.tbxMail.Location = new System.Drawing.Point(549, 89);
+            this.tbxMail.Name = "tbxMail";
+            this.tbxMail.Size = new System.Drawing.Size(162, 20);
+            this.tbxMail.TabIndex = 3;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(503, 282);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(208, 108);
+            this.btnModifier.TabIndex = 4;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // ModifierParametre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.tbxMail);
+            this.Controls.Add(this.lblmail);
+            this.Controls.Add(this.cbxProd);
+            this.Controls.Add(this.gbxInfo);
+            this.Name = "ModifierParametre";
             this.Text = "ModifierParametre";
+            this.Load += new System.EventHandler(this.ModifierParametre_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbxInfo;
+        private System.Windows.Forms.CheckBox cbxProd;
+        private System.Windows.Forms.Label lblmail;
+        private System.Windows.Forms.TextBox tbxMail;
+        private System.Windows.Forms.Button btnModifier;
     }
 }
--- projetavecDB/ModifierParametre.cs
+++ projetavecDB/ModifierParametre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,148 @@ namespace projetavecDB
         public ModifierParametre()
         {
             InitializeComponent();
+        }
+
+        private void ModifierParametre_Load(object sender, EventArgs e)
+        {
+            MySqlConnection maCnx = new MySqlConnection("server=localhost;user=root;database=Atlantik;port=3306;password=");
+            MySqlDataReader jeuEnr = null;
+            try
+            {
+                Label lblSite, lblRang, lblIdentifiant, lblCléHMAC ;
+                TextBox tbxSite, tbxRang, tbxIdentifiant, tbxCléHMAC;
+                int i = 2;
+                maCnx.Open();
+                string requête = "select * from parametres";
+                var maCde = new MySqlCommand(requête, maCnx);
+                jeuEnr = maCde.ExecuteReader();
+
+                while (jeuEnr.Read())
+                {
+
+                    string Site =(string)jeuEnr["site_pb"];
+                    string rang =(string)jeuEnr["rang_pb"];
+                    string identifiant = (string)jeuEnr["identifiant_pb"];
+                    string cleHMAC = (string)jeuEnr["cleHMAC_pb"];
+                    bool prod = cbxProd.Checked;
+
+
+
+                    lblSite = new Label();
+                    lblSite.Text = "Site :";
+                    lblSite.Location = new Point(15, 25 * i);
+                    lblSite.AutoSize = true;
+                    gbxInfo.Controls.Add(lblSite);
+
+                    tbxSite = new TextBox();
+                    tbxSite.Text = Site;
+                    tbxSite.Location = new Point(150, 25 * i);
+                    tbxSite.AutoSize = true;
+                    gbxInfo.Controls.Add(tbxSite);
+
+                    i++;
+                    lblRang = new Label();
+                    lblRang.Text = "Rang :";
+                    lblRang.Location = new Point(15, 25 * i);
+                    lblRang.AutoSize = true;
+                    gbxInfo.Controls.Add(lblRang);
+
+                    tbxRang = new TextBox();
+                    tbxRang.Text = rang;
+                    tbxRang.Location = new Point(150, 25 * i);
+                    tbxRang.AutoSize = true;
+                    gbxInfo.Controls.Add(tbxRang);
+
+                    i++;
+
+                    lblIdentifiant = new Label();
+                    lblIdentifiant.Text = "Identifiant :";
+                    lblIdentifiant.Location = new Point(15, 25 * i);
+                    lblIdentifiant.AutoSize = true;
+                    gbxInfo.Controls.Add(lblIdentifiant);
+
+                    tbxIdentifiant = new TextBox();
+                    tbxIdentifiant.Text = identifiant;
+                    tbxIdentifiant.Location = new Point(150, 25 * i);
+                    tbxIdentifiant.AutoSize = true;
+                    gbxInfo.Controls.Add(tbxIdentifiant);
+
+                    i++;
+
+                    lblCléHMAC = new Label();
+                    lblCléHMAC.Text = "Clé HMAC :";
+                    lblCléHMAC.Location = new Point(15, 25 * i);
+                    lblCléHMAC.AutoSize = true;
+                    gbxInfo.Controls.Add(lblCléHMAC);
+
+                    tbxCléHMAC = new TextBox();
+                    tbxCléHMAC.Text = cleHMAC;
+                    tbxCléHMAC.Location = new Point(150, 25 * i);
+                    tbxCléHMAC.AutoSize = true;
+                    gbxInfo.Controls.Add(tbxCléHMAC);
+
+
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erreur " + ex.ToString());
+            }
+            finally
+            {
+                if (jeuEnr is object & !jeuEnr.IsClosed)
+                {
+                    jeuEnr.Close(); // s'il existe et n'est pas déjà fermé
+                }
+
+                if (maCnx is object & maCnx.State == ConnectionState.Open)
+                {
+                    maCnx.Close(); // on se déconnecte
+
+                }
+            }
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            MySqlConnection maCnx = new MySqlConnection("server=localhost;user=root;database=Atlantik;port=3306;password=");
+            MySqlDataReader jeuEnr = null;
+            try
+            {
+                Label lblnom;
+                TextBox txtValeur;
+                int i = 2;
+                maCnx.Open();
+                string requête = "update parametres set site_pb = @site,rang_pb = rang, identifiant_pb = @identifiant, cleHMAC_pb = @cleHMAC";
+                var maCde = new MySqlCommand(requête, maCnx);
+                jeuEnr = maCde.ExecuteReader();
+
+                foreach (Control c in gbxInfo.Controls)
+                {
+                    if (c is TextBox tbx)
+                    {
+
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erreur " + ex.ToString());
+            }
+            finally
+            {
+                if (jeuEnr is object & !jeuEnr.IsClosed)
+                {
+                    jeuEnr.Close(); // s'il existe et n'est pas déjà fermé
+                }
+
+                if (maCnx is object & maCnx.State == ConnectionState.Open)
+                {
+                    maCnx.Close(); // on se déconnecte
+
+                }
+            }
         }
     }
 }
