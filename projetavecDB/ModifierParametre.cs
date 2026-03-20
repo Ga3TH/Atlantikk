94225e7f42877a7b04d42be78566d6acc0a28cec
gaeth-hub <gaeth0311@gmail.com>
2026-03-20 13:58:36 +0100

    finito pipopo

--- projetavecDB/ModifierParametre.cs
+++ projetavecDB/ModifierParametre.cs
@@ -54,6 +54,46 @@ namespace projetavecDB
 
         private void btnModifier_Click(object sender, EventArgs e)
         {
+            var objetRegEx = new Regex("^[0-9]+$");
+            var objetRegExMel = new Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+            if (tbxSite.Text == "")
+            {
+                tbxSite.BackColor = Color.Red;
+                pbSaisie.SetError(tbxSite, "Veuillez saisir un site !");
+                return;
+            }
+            if (tbxRang.Text == "" || !objetRegEx.IsMatch(tbxRang.Text))
+            {
+                tbxRang.BackColor = Color.Red;
+                pbSaisie.SetError(tbxRang, "Veuillez saisir un rang entier valide !");
+                return;
+            }
+            if (tbxIdentifiant.Text == "" || !objetRegEx.IsMatch(tbxIdentifiant.Text))
+            {
+                tbxIdentifiant.BackColor = Color.Red;
+                pbSaisie.SetError(tbxIdentifiant, "Veuillez saisir un identifiant entier valide !");
+                return;
+            }
+            if (tbxCleHMAC.Text == "")
+            {
+                tbxCleHMAC.BackColor = Color.Red;
+                pbSaisie.SetError(tbxCleHMAC, "Veuillez saisir une clé HMAC !");
+                return;
+            }
+            if (tbxMail.Text == "" || !objetRegExMel.IsMatch(tbxMail.Text))
+            {
+                tbxMail.BackColor = Color.Red;
+                pbSaisie.SetError(tbxMail, "Mail invalide !");
+                return;
+            }
+
+            tbxSite.BackColor = Color.Green;
+            tbxRang.BackColor = Color.Green;
+            tbxIdentifiant.BackColor = Color.Green;
+            tbxCleHMAC.BackColor = Color.Green;
+            tbxMail.BackColor = Color.Green;
+
             MySqlConnection maCnx = new MySqlConnection("server=localhost;user=root;database=Atlantik;port=3306;password=");
             MySqlDataReader jeuEnr = null;
             try
@@ -97,13 +137,12 @@ namespace projetavecDB
 
         private void tbxRang_Validating(object sender, CancelEventArgs e)
         {
-            var objetRegEx = new Regex("^[0-9]*$");
-            var résultatTest = objetRegEx.Match(tbxIdentifiant.Text);
-            if (!résultatTest.Success)
+            var objetRegEx = new Regex("^[0-9]+$");
+            if (tbxRang.Text == "" || !objetRegEx.IsMatch(tbxRang.Text))
             {
                 tbxRang.BackColor = Color.Red;
                 e.Cancel = true;
-                pbSaisie.SetError(tbxRang, "Veuillez saisir un rang !");
+                pbSaisie.SetError(tbxRang, "Veuillez saisir un rang entier valide !");
             }
             else
             {
@@ -111,16 +150,14 @@ namespace projetavecDB
                 pbSaisie.SetError(tbxRang, "");
             }
         }
-
         private void tbxIdentifiant_Validating(object sender, CancelEventArgs e)
         {
-            var objetRegEx = new Regex("^[0-9]*$");
-            var résultatTest = objetRegEx.Match(tbxIdentifiant.Text);
-            if (!résultatTest.Success)
+            var objetRegEx = new Regex("^[0-9]+$");
+            if (tbxIdentifiant.Text == "" || !objetRegEx.IsMatch(tbxIdentifiant.Text))
             {
                 tbxIdentifiant.BackColor = Color.Red;
                 e.Cancel = true;
-                pbSaisie.SetError(tbxIdentifiant, "Veuillez saisir un identifiant !");
+                pbSaisie.SetError(tbxIdentifiant, "Veuillez saisir un identifiant entier valide !");
             }
             else
             {
@@ -147,11 +184,11 @@ namespace projetavecDB
         private void tbxMail_Validating(object sender, CancelEventArgs e)
         {
             var objetRegExMel = new Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
-            if (!objetRegExMel.Match(tbxMail.Text).Success || tbxMail.Text == "")
+            if (tbxMail.Text == "" || !objetRegExMel.IsMatch(tbxMail.Text))
             {
                 tbxMail.BackColor = Color.Red;
                 e.Cancel = true;
-                pbSaisie.SetError(tbxMail, "Mail invalide");
+                pbSaisie.SetError(tbxMail, "Mail invalide !");
             }
             else
             {
